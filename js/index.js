609339afb0e27b9a9475fe7f17a793f6fd97079a
fengashlie <fengashlie@yahoo.com>
2022-07-24 17:25:35 -0500

    First Upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+ScrollReveal({ duration:1000 })
+
+ScrollReveal().reveal(".navigation", {delay:500});
+ScrollReveal().reveal(",heroImg", {delay:500});
+ScrollReveal().reveal(".mySkills", {delay:500});
+ScrollReveal().reveal(".caseStudies", {delay:500});
